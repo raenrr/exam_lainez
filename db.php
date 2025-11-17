66ddf5f89562ea4b011a07531bca63869a85775a
raenrr <raenomg@gmail.com>
2025-11-17 14:07:50 +0800

    Change database name from exam_lainez to examdb

--- db.php
+++ db.php
@@ -2,11 +2,12 @@
 $host = "localhost";
 $user = "root"; 
 $pass = ""; 
-$db = "exam_lainez";
+$db = "examdb";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
